2e581d8dccf9d2b49f039975efa8b6950649c90f
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-11-14 12:42:37 +0800

    Move to WebApi

--- src/WebApi/Extensions/SwaggerGenOptionsExtensions.cs
+++ src/WebApi/Extensions/SwaggerGenOptionsExtensions.cs
@@ -8,7 +8,8 @@ namespace Nova.Common
     {
         public static void UseNovaSchemaIds(this SwaggerGenOptions options)
         {
-            options.CustomSchemaIds(type => {
+            options.CustomSchemaIds(type => 
+            {
                 var schemaIdAttribs = type.GetCustomAttributes(typeof(SchemaIdAttribute), false);
 
                 if (schemaIdAttribs.Any() && schemaIdAttribs[0] is SchemaIdAttribute schemaId)
