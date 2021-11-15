d77b4dcad6ad54744e043f13849b72c75d60cb34
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-11-15 18:25:58 +0800

    Added WebApi to packaging script

--- nuget_pack.ps1
+++ nuget_pack.ps1
@@ -1,2 +1,3 @@
 dotnet pack src/Core --configuration Release --output ../.nuget_releases/
 dotnet pack src/Server --configuration Release --output ../.nuget_releases/
+dotnet pack src/WebApi --configuration Release --output ../.nuget_releases/
